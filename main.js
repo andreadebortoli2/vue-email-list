35bfc98993b3870c0a157ebba86b29631dd2f32a
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-03-19 18:46:32 +0100

    print address on page

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
-console.log('test');
+// console.log('test');
+
+/* 
+--Descrizione:
+- Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail 
+- generare 10 indirizzi email 
+- e stamparli in pagina all'interno di una lista.
+--Bonus
+- Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
 
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-      message: 'Hello Vue!'
-    }
-  }
+    data() {
+        return {
+            emailAddresses: []
+        }
+    },
+    methods: {
+    },
+    created() {
+        // console.log(this.emailAddresses);
+        for (let i = 0; i < 10; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // console.log(response.data.response);
+                this.emailAddresses.push(response.data.response);
+            })  
+        }
+        // console.log(this.emailAddresses);
+    },
+    mounted() {
+    },
 }).mount('#app')
